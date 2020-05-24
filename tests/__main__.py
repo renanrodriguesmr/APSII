870aa849dac0dcf9fa9f7f759dcabe26a6c8ab53
mgirardin <matheus.girardin@gmail.com>
2020-05-24 18:52:00 -0300

    Creating instance type tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,7 @@
 from tests.main import TestMain
 from tests.models.dataset.escolas import TestEscolasDataSet
+from tests.models.dataset.ideb_brasil import TestIDEBBrasilDataSet
+from tests.models.dataset.ideb_escolas import TestIDEBEscolasDataSet
 import unittest
 import unittest.mock
 
